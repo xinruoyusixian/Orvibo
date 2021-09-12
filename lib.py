a8a83c00fd9e7e3ccfd07a1b4e071a3824ca53bb
xw <myxw94@gmail.com>
2021-09-12 21:20:06 +0800

    目录功能更新重写

--- lib.py
+++ lib.py
@@ -1,79 +1,7 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#PWM 工作模板#
-#pwm0 = PWM(Pin(0))      # 通过Pin对象来创建PWM对象
-#pwm0.freq()             # 获得当前的PWM频率
-#pwm0.freq(1000)         # 设置PWM频率
-#pwm0.duty()             # 获得当前的PWM占空比
-#pwm0.duty(200)          # 设置占空比
-#PWM 简写
-#pwm2 = PWM(Pin(2), freq=200, duty=1099)# 创建PWM同时设置参数
-#pwm0.deinit()           # 关闭PWM
-#上传温度
-import  urequests
 import network
 from machine import Pin, PWM ,RTC,Timer
-import time,dht,machine,ujson,ntptime,sys
-
-
-
-
-#测温度
-def dhts(pin,dh=11):
-  if dh==11:
-    d=dht.DHT11(machine.Pin(pin))
-  if dh==22:
-    d=dht.DHT22(machine.Pin(pin))
-  a=[]
-  try:
-      d.measure() 
-      time.sleep(0.5)
-      #print ("---------------------")
-      #print ("温度", d.temperature() )
-      #print ("湿度" ,d.humidity())
-      a=[d.temperature(),d.humidity()]
-    #except OSError as e:
-  except Exception as e:
-       #print ("出错")
-       a=['err','err']
-       pass
-  return a
-
-
-
-
+import time,machine,ntptime,sys
 def ap(ssd,pwd=''):
     AP= network.WLAN(network.AP_IF)
     if ssd=='':
@@ -164,7 +92,6 @@ class flashLed:
 
 
 
-
 def update_time():
 
       ntptime.host='ntp1.aliyun.com'
@@ -189,15 +116,10 @@ def wifi(ssd='',pwd='',hostname="MicroPython"):
       wifi0.disconnect()
       _s_time=time.time()
       if not wifi0.isconnected(): #判断WIFI连接状态
-
           print('[WIFI]:Connect to',ssd)
-
           wifi0.connect(ssd, pwd) #essid为WIFI名称,password为WIFI密码
-
           while not wifi0.isconnected():
-
             if (time.time()- _s_time)>5:
-
               print('[WIFI]:Connect Faied')
               return (wifi0,False)
     
@@ -259,62 +181,3 @@ class btn:
           self.cb_click()
         self.time=0
         return  
-        
-#网络检测 
-def  isOline():
-  try:
-   urequests.get("http://www.baidu.com")
-  except:
-   return False
-  return True
-
-class  _wifi:
- 
-  def __init__(self,ssd,pwd):
-    self.ssd=ssd
-    self.pwd=pwd
-    self.hostname="micropython" 
-    self.wifi0 = network.WLAN(network.STA_IF)  
-  def connect(self):
-    self.wifi0.active(True) #激活WIFI
-    self.wifi0.config(dhcp_hostname=self.hostname)
-    if not self.wifi0.isconnected(): #判断WIFI连接状态
-
-          print('[wifi]:正在连接...')
-
-          self.wifi0.connect(self.ssd, self.pwd) #essid为WIFI名称,password为WIFI密码
-          time.sleep(1)
-          if self.wifi0.isconnected():
-
-              pass # WIFI没有连接上的话做点什么,这里默认pass啥也不做
-
-    print('network config[网络信息]:', self.wifi0.ifconfig())    
-
-  def mdns(self,host=0):
-    self.disconnect()
-    self.hostname=host
-    print(host,".local")
-    time.sleep(1)
-    self.connect()
-  def disconnect(self):
-    self.wifi0.disconnect()
-    
-  def info(self):
-    return self.wifi0.ifconfig()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
